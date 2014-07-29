0c52a8a597025852f5a9698c49a99bec6cc41dcb
Brian Sipple <Bsipple57@gmail.com>
2014-07-29 18:59:00 -0500

    update functional tests

--- superlists/functional-test.py
+++ superlists/functional-test.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Helga has heard about a cool new online to-do app. She goes
+# to check out its homepage
 browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+# She notices the page title and header mention to-do lists
+assert "To-Do" in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy orange spools" into a text box (Helga's hobby
+# is 3D-Printing)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy orange spools" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Buy orange spools to make a coffee mug (Helga is also a coffee connoisseur)
+#
+#
+# The page updates again, and now shows both items on her list
+
